ce103b6335c57a641b46c18c11aac21f0ff74975
Fabian Wahlster <razor@singul4rity.com>
2017-12-06 11:37:30 +0100

    execute copy_boost_libs.ps1 once after boostrap & bjam is done

---
+++ Source/utilscripts/copy_boost_libs.ps1
@@ -0,0 +1,33 @@
+
+#[string]$SolutionDir = "C:\Projects\Tracy\Source\"
+[string]$SolutionDir = "..\"
+[string]$BoostBinReleaseDir = ($SolutionDir + "boost\bin\release\")
+[string]$BoostBinDebugDir = ($SolutionDir + "boost\bin\debug\")
+[string]$BoostInputFiles = ($SolutionDir + "boost\bin.v2\libs\")
+
+
+if (!(Test-Path -path $BoostBinReleaseDir))
+{
+    New-Item $BoostBinReleaseDir -Type Directory
+}
+
+if (!(Test-Path -path $BoostBinDebugDir))
+{
+    New-Item $BoostBinDebugDir -Type Directory
+}
+
+foreach ($file in get-ChildItem -recurse -path $BoostInputFiles -include *.lib)
+{
+    if($file.Name.Contains("x64"))
+    {
+        Echo ("Copying " + $file.Name)
+        if($file.FullName.Contains("debug"))
+        {        
+           Copy-Item $file $BoostBinDebugDir
+        }
+        if($file.FullName.Contains("release"))
+        {
+           Copy-Item $file $BoostBinReleaseDir    
+        }
+    }
+}
